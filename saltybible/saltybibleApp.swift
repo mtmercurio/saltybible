181664745d38b619404bd35866931575d10c5a02
toreymercurio <mtmercurio@gmail.com>
2023-01-27 12:33:13 -0500

    Initial Commit

---
+++ saltybible/saltybibleApp.swift
@@ -0,0 +1,17 @@
+//
+//  saltybibleApp.swift
+//  saltybible
+//
+//  Created by Torey Mercurio on 1/27/23.
+//
+
+import SwiftUI
+
+@main
+struct saltybibleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
